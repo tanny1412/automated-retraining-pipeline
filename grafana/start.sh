39a5575e756c5730b6d62a277480bff27c5a8bd7
Tanish Kandivlikar <tanish.kandivlikar1412@gmail.com>
2026-05-09 22:14:29 -0400

    Revert "auto-generate Grafana contact-points.yml from template at startup"
    This reverts commit 65a48d1e0efc0b9f0ecf9bd62a20581238de5cc6.

--- grafana/start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-sed "s|SLACK_WEBHOOK_URL_PLACEHOLDER|${SLACK_WEBHOOK_URL}|g" \
-    /etc/grafana/provisioning/alerting/contact-points.yml.template \
-    > /etc/grafana/provisioning/alerting/contact-points.yml
-exec /run.sh
